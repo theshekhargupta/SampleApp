e8d4dedc613cfc6076c6ee947c670e14a3c55830
theshekhargupta <theshekhargupta@gmail.com>
2020-01-03 18:48:38 +0530

    Added slider menu in Dashboard

--- SampleApp/Modules/DashBoard/DashboardViewController.swift
+++ SampleApp/Modules/DashBoard/DashboardViewController.swift
@@ -13,6 +13,37 @@ class DashboardViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        self.configureUI()
+    }
+    
+    func configureUI() {
+        self.configureNavigationBar()
+        self.addBackButton()
+    }
+    
+    func configureNavigationBar() {
+        let burgerMenuIcon = UIButton(type: .custom)
+        burgerMenuIcon.setImage(UIImage(named: "burgerMenu"), for: .normal)
+        burgerMenuIcon.addTarget(self, action: #selector(self.showSlideMenu), for: .touchUpInside)
+        self.navigationItem.leftBarButtonItem = UIBarButtonItem(customView: burgerMenuIcon)
+    }
+    
+    func addBackButton() {
+        let backButton = UIButton(type: .custom)
+        backButton.setImage(UIImage(named: "burgerMenu.png"), for: .normal) // Image can be downloaded from here below link
+        backButton.setTitle("Menu", for: .normal)
+        backButton.setTitleColor(backButton.tintColor, for: .normal) // You can change the TitleColor
+        backButton.addTarget(self, action: #selector(self.showSlideMenu), for: .touchUpInside)
+
+        self.navigationItem.leftBarButtonItem = UIBarButtonItem(customView: backButton)
+    }
+
+    @IBAction func backAction(_ sender: UIButton) {
+       let _ = self.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func showSlideMenu() {
+        slideMenuController()?.openLeft()
     }
     
     @IBAction func showDetailView() {
@@ -20,5 +51,4 @@ class DashboardViewController: UIViewController {
         let vc = storyboard.instantiateViewController(withIdentifier: "DetailedViewController")
         self.navigationController?.push(vc, animated: true)
     }
-
 }
